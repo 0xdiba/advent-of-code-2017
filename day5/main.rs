1a36fcd1f730ef3fa3cfb74b1848a296d9eb2992
Dimitris Bachtis <dimitris.bachtis@skroutz.gr>
2017-12-05 17:57:49 +0200

    day 5: part 1

---
+++ day5/main.rs
@@ -0,0 +1,27 @@
+
+use std::fs::File;
+use std::io::{BufReader,BufRead};
+
+fn main() {
+    let f = File::open("./input.txt").expect("file not found");
+
+    let mut jumps = Vec::new();
+    for line in BufReader::new(f).lines() {
+        let n = line.unwrap().parse::<i32>().unwrap();
+        jumps.push(n);
+    }
+
+    let mut pos = 0; 
+    let mut no_jumps = 0;
+    loop {
+        no_jumps += 1;
+        let next = jumps[pos];
+        jumps[pos] = jumps[pos] + 1; 
+        pos = (pos as i32 + next) as usize;
+
+        if pos >= jumps.len() {
+            break;
+        }
+    }
+    println!("Jumps: {}", no_jumps)
+}
